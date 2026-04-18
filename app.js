52c436f466b37c85b3aeb168bea66d266ce05d5a
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-04-18 14:56:33 -0400

    add lesson 5

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Lesson 3 - Practice: Array Methods
+
 // Task 1
 let fruits = ["apple", "banana", "cherry"];
 fruits.push("orange");
@@ -31,3 +33,43 @@ let scores = [85, 70, 95, 60, 75];
 scores.sort((a, b) => a - b); // sort asc order
 scores.reverse();
 console.log(scores);
+
+/* -------------------------------------------------------------------------- */
+
+// Lesson 5 - Practice: Array Iterator Methods
+
+// Task 1
+const favoriteCities = ["New York", "London", "Tokyo", "Paris", "Sydney"];
+favoriteCities.forEach((city) => {
+	console.log(city.toUpperCase()); // NEW YORK, LONDON, TOKYO, PARIS, SYDNEY
+});
+
+// Task 2
+const numbers2 = [1, 2, 3, 4, 5];
+const squares = numbers2.map((num) => num * num); // [1, 4, 9, 16, 25]
+console.log(squares);
+
+// Task 3
+const scores2 = [85, 42, 90, 75, 30, 100];
+const highScores = scores2.filter((score) => score >= 80); // [85, 90, 100]
+console.log(highScores);
+
+// Task 4
+const favoriteFood = ["grilled cheese", "sushi", "rice", "ramen", "curry"];
+const firstLongWord = favoriteFood.find((food) => food.length > 4);
+const firstLongWordIndex = favoriteFood.findIndex((food) => food.length > 4);
+console.log(firstLongWord, firstLongWordIndex); // grilled cheese 0
+
+// Task 5
+const temperatures = [72, 85, 95, 68, 78];
+const above90 = temperatures.some((temp) => temp > 90);
+const allAbove50 = temperatures.every((temp) => temp > 50);
+console.log([above90, allAbove50]); // [true, true]
+
+// Task 6
+const totalBudget = 100;
+const prices = [25, 15, 10, 20];
+const remainingBudget = prices.reduce((budget, price) => {
+	return budget - price;
+}, totalBudget);
+console.log(remainingBudget); // 30 (remaining budget after purchasing items)
